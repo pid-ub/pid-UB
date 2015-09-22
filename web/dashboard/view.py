067e34fa8298739f3b0328c9991277521b41073a
xaviml <xaviml.93@gmail.com>
2015-09-23 01:50:57 +0200

    Add another plot to debug

--- web/dashboard/view.py
+++ web/dashboard/view.py
@@ -6,14 +6,15 @@ from model.PlotModel import PlotModel
 
 
 def simple_chart(request):
-    figures = []
     # Mocked plot
     f = figure()
     f.circle([1, 2, 3, 4, 5], [2, 5, 8, 2, 7], size=10)
+    f2 = figure()
+    f2.circle([2, 5, 8, 2, 7], [1, 2, 3, 4, 5], size=5)
     # Insert in figures the plots given from service
-    figures.append(f)
+    figures = [f, f2]
 
-    script, plots = plot(["myPlot"], figures)
+    script, plots = plot(["myPlot", "myOtherPlot"], figures)
     return render(request, "test.html", {"script": script, "plots": plots})
 
 
