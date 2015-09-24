3a55fb2461f35d3e1182f06d67565afebfea10ed
xaviml <xaviml.93@gmail.com>
2015-09-24 20:25:28 +0200

    change settings.py

--- web/settings.py
+++ web/settings.py
@@ -136,8 +136,42 @@ WSGI_APPLICATION = 'wsgi.application'
 import numpy as np
 import pandas as pd
 
+#import from utils
+import sys
+sys.path.append('../questions')
+from utils import *
+
 reg = pd.read_csv("../../registers.csv", na_values=np.nan)
 assig = pd.read_csv("../../assigs.csv", na_values=np.nan)
 conv = pd.read_csv("../../conv.csv", na_values=np.nan)
 qual = pd.read_csv("../../qualifications.csv", na_values=np.nan)
 qual_norm = pd.read_csv("../../qualifications_normalized.csv", na_values=np.nan)
+
+reg_info = reg[reg['id_enseny'] != 'G1042']
+
+# Treiem les de mates (G1042)
+assig_info = assig[assig['id_enseny_assig'] != 'G1042']
+
+df_info = pd.merge(pd.merge(qual, assig_info), reg_info)
+
+s1_info = [364288, 364289, 364290, 364291, 364292, 364293, 364294, 364298, 364299, 364301]
+s1_info_lbl = ['P1', 'DDB', 'IO', 'ALGE', 'CAL', 'MD', 'FIS', 'ALGO', 'P2', 'ED']
+s2_info = [364297, 364300, 364303, 364305, 364302, 364296, 364295, 364306, 364304, 364307]
+s2_info_lbl = ['ELEC', 'AA', 'DS', 'EC', 'ICC', 'EMP', 'PIE', 'PAE', 'PIS', 'SO1']
+s3_info = [364314, 364308, 364322, 364315, 364309, 364311, 364323, 364328, 364310, 364312]
+s3_info_lbl = ['IA', 'SO2', 'TNUI', 'VA', 'XAR', 'BD', 'FHIC', 'GiVD', 'LIL', 'SWD']
+
+primer_info_qual = table_students(df_info, s1_info)
+segon_info_qual = table_students(df_info, s2_info)
+primer_info_qual.columns = s1_info_lbl
+segon_info_qual.columns = s2_info_lbl
+
+df = select_rows_by_columns(df_info, primer_info_qual.index, 'id_alumne')
+via_0 = df[df['id_via_acces'] ==  0].reset_index(drop=True) # Batxillerat/COU amb PAU
+#via_2 = df[df['id_via_acces'] ==  2].reset_index(drop=True) # Diplomat, Llicenciat
+#via_4 = df[df['id_via_acces'] ==  4].reset_index(drop=True) # FP2, CFGS
+via_7 = df[df['id_via_acces'] ==  7].reset_index(drop=True) # Universitaris Batxillerat/COU amb PAU
+via_8 = df[df['id_via_acces'] ==  8].reset_index(drop=True)#  Universitaris FP2 / CFGS
+via_78 = pd.concat([via_7, via_8]) # Universitaris
+via_acces_info_desc = ['Batx', 'Uni']
+via_acces_info = [via_0['id_alumne'], via_78['id_alumne']]
