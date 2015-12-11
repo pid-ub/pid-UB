420d4772d1a2c90d0fae747bdcf147a90ab5ec3d
xaviml <xaviml.93@gmail.com>
2015-12-11 18:59:13 +0100

    Done, theme changed

--- web/dashboard/view.py
+++ web/dashboard/view.py
@@ -8,23 +8,10 @@ from service.clustering import clustering, estimate, renounce
 from service.asignaturas import asignaturas
 from settings import *
 
-
-def dashboard_view(request):
+def home_view(request):
     # Insert in figures the plots given from service
     figures = []
     figures_lbl = []
-
-    if str(request) == "<WSGIRequest: GET '/dashboard_asignaturas/'>":
-        figura_regresion, figura_evolucion, figura_convalidacion = asignaturas(reg, qual, assig)
-        figures.append(figura_regresion)
-        figures_lbl.append("regresion")
-        figures.append(figura_evolucion)
-        figures_lbl.append("evolucion")
-        figures.append(figura_convalidacion)
-        figures_lbl.append("convalidacion")
-
-        script, plots = plot(figures_lbl, figures)
-        return render(request, "dashboard_asignaturas.html", {"script": script, "plots": plots})
 
     tmp = estimate(primer_info_qual, 5)
     lbl_clusters = ["MP1", # Milors en programació de primer
@@ -55,8 +42,17 @@ def dashboard_view(request):
     figures.append(renounce(tmp, colors, df_info, s3_info, lbl_clusters))
     figures_lbl.append("renounce2")
 
+
+    figura_regresion, figura_evolucion, figura_convalidacion = asignaturas(reg, qual, assig)
+    figures.append(figura_regresion)
+    figures_lbl.append("regresion")
+    figures.append(figura_evolucion)
+    figures_lbl.append("evolucion")
+    figures.append(figura_convalidacion)
+    figures_lbl.append("convalidacion")
+
     script, plots = plot(figures_lbl, figures)
-    return render(request, "dashboard.html", {"script": script, "plots": plots})
+    return render(request, "index.html", {"script": script, "plots": plots})
 
 
 def plot(keys, f):
--- web/urls.py
+++ web/urls.py
@@ -1,6 +1,6 @@
-from django.conf.urls import patterns, include, url
-from django.views.generic import TemplateView, RedirectView
-from dashboard.view import dashboard_view
+from django.conf.urls import patterns, url
+from django.views.generic import RedirectView
+from dashboard.view import home_view
 
 # Uncomment the next two lines to enable the admin:
 from django.contrib import admin
@@ -8,17 +8,8 @@ admin.autodiscover()
 
 urlpatterns = patterns(
     '',
-    url(r'^dashboard/$', dashboard_view, name="dashboard"),
-    url(r'^dashboard_asignaturas/$', dashboard_view, name="dashboard_asignaturas"),
-    url(r'^other/$', dashboard_view, name="other"),
-    url(r'^about/$', TemplateView.as_view(template_name='about.html'), name="about"),
-    url(r'^$', RedirectView.as_view(pattern_name='dashboard', permanent=False))
-    # Examples:
-    # url(r'^$', 'dashboard.views.home', name='home'),
-    # url(r'^dashboard/', include('dashboard.foo.urls')),
-
-    # Uncomment the admin/doc line below to enable admin documentation:
-    # url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
+    url(r'^home/$', home_view, name="home"),
+    url(r'^$', RedirectView.as_view(pattern_name='home', permanent=False))
 )
 
 # Uncomment the next line to serve media files in dev.
